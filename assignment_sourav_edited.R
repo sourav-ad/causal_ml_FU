753fb3bd8a49f59d148eff85a3d175572b776d87
Adhikari <adhikari@hwr-berlin.de>
2026-01-07 13:18:09 +0100

    fixes, comments, almost ready

--- assignment_sourav_edited.R
+++ assignment_sourav_edited.R
@@ -212,19 +212,22 @@ controls_unproblematic <- c(
 
 
 # outcome is for classification
-data_clean$sportsclub_f <- factor(
-  data_clean$sportsclub,
+data_clean$health1_f <- factor(
+  data_clean$health1,
   levels = c(0, 1),
   labels = c("No", "Yes")
 )
 
-#logistic regression
+#logistic regression, still conditions on the treatment, additionally on
+#unproblematic covariates
 form_logit <- as.formula(
-  paste("sportsclub_f ~", paste(controls_unproblematic, collapse = " + "))
+  paste("health1_f ~ sportsclub +",  #health1 as yes/no
+        paste(controls_unproblematic, collapse = " + "))
 )
 
 #cross validation 
 
+#10 fold
 ctrl_10 <- trainControl(
   method = "cv",
   number = 10
@@ -240,9 +243,11 @@ logit_10 <- train(
 
 logit_10
 
-#Accuracy   Kappa    
-#0.6144949  0.1814244
+#  Accuracy   Kappa    
+# 0.7276163  -3.715547e-05
 
+
+#5 fold
 
 ctrl_5 <- trainControl(
   method = "cv",
@@ -260,13 +265,13 @@ logit_5 <- train(
 logit_5
 
 # Accuracy   Kappa    
-# 0.6136358  0.1802731
+# 0.7276162  0.0003451815
 
 cv_10_error <- 1 - logit_10$results$Accuracy
 cv_5_error  <- 1 - logit_5$results$Accuracy
 
-cv_10_error # 0.3855051 ; lower bias, higher variance
-cv_5_error # 0.3863642 ; higher bias, lower variance
+cv_10_error # 0.2723837 
+cv_5_error # 0.2723838
 
 
 ### (b) ####
@@ -288,11 +293,11 @@ nrow(data_train) / nrow(data_clean)  # 0.6999657
 nrow(data_test)  / nrow(data_clean)  # 0.3000343
 
 X_train <- model.matrix(
-  as.formula(paste("sportsclub ~", paste(controls_unproblematic, collapse = " + "))),
+  as.formula(paste("health1 ~ sportsclub +", paste(controls_unproblematic, collapse = " + "))),
   data_train
 )[, -1]   # drop intercept
 
-y_train <- data_train$sportsclub
+y_train <- data_train$health1
 
 #Using 5-fold cv
 
@@ -307,7 +312,7 @@ cv_lasso <- cv.glmnet(
 
 #the best lambda parameter
 lambda_lasso <- cv_lasso$lambda.min
-lambda_lasso # 0.001194322
+lambda_lasso # 0.001394045
 
 #final model with optimal lambda
 lasso_model <- glmnet(
@@ -330,7 +335,7 @@ cv_ridge <- cv.glmnet(
 )
 
 lambda_ridge <- cv_ridge$lambda.min
-lambda_ridge # 0.009464796
+lambda_ridge # 0.008357083
 
 #final model
 
@@ -354,7 +359,7 @@ cv_elastic <- cv.glmnet(
 )
 
 lambda_elastic <- cv_elastic$lambda.min
-lambda_elastic # 0.001806921
+lambda_elastic # 0.002788091
 
 #final model with best lambda
 elastic_model <- glmnet(
@@ -365,31 +370,136 @@ elastic_model <- glmnet(
   lambda = lambda_elastic
 )
 
-## ALTERNATIVE (EDI): Grid search for alpha and lambda:
-set.seed(123)
-cv_elastic <- cva.glmnet(
-  X_train,
-  y_train,
-  alpha = seq(0, 1, 1/100),
-  family = "binomial",
-  nfolds = 5
-)
+# Grid search for alpha and lambda:
+#set.seed(123)
 
-min_cvm <- sapply(cv_elastic$modlist, function(m) min(m$cvm))
-best_i <- which.min(min_cvm)
-
-alpha_elastic <- cv_elastic$alpha[best_i]
-lambda_elastic <- cv_elastic$modlist[[best_i]]$lambda.min
-
-alpha_elastic # 0 - basically Ridge
-lambda_elastic #  0.008623969 - close to Lambda ridge
-
-range(min_cvm)
-# 1.309946 1.309998
+# cv_elastic_grid <- cva.glmnet(
+#   X_train,
+#   y_train,
+#   alpha = seq(0, 1, 1/100),
+#   family = "binomial",
+#   nfolds = 5
+# )
+# 
+# min_cvm <- sapply(cv_elastic_grid$modlist, function(m) min(m$cvm))
+# best_i <- which.min(min_cvm)
+# 
+# alpha_elastic_grid <- cv_elastic_grid$alpha[best_i]
+# lambda_elastic_grid <- cv_elastic_grid$modlist[[best_i]]$lambda.min
+# 
+# alpha_elastic_grid # 1 - basically lasso
+# lambda_elastic_grid #  0.001394045 - exactly lambda_lasso
+# 
+# range(min_cvm)
+# 1.136796 1.136904
 # Very close range, suggests that all alphas perform very similarly
 
 # Sould we go with this? Or keep this but then justify fixing alpha at 0.5 just
 # to present a "different" model?
+
+#Sourav: If I strictly interpret the question, they mention "penalty parameter"
+# lambda is the penalty parameter
+# alpha controls the mixing of L1 and L2
+# so may be this is a bit overkill? But we can keep it if you want. Now it 
+#looks pure lasso
+
+# (c) Evaluate all three models on the test set and compare their prediction errors.
+
+#test data
+
+X_test <- model.matrix(
+  as.formula(
+    paste(
+      "health1 ~ sportsclub +",
+      paste(controls_unproblematic, collapse = " + ")
+    )
+  ),
+  data_test
+)[, -1]
+
+y_test <- data_test$health1
+
+#make predictions, predicted probabilities
+p_lasso <- predict(cv_lasso, newx = X_test, s = "lambda.min", type = "response")
+p_ridge <- predict(cv_ridge, newx = X_test, s = "lambda.min", type = "response")
+p_elastic <- predict(cv_elastic, newx = X_test, s = "lambda.min", alpha = 0.5, type = "response")
+
+#convert to class predictions
+yhat_lasso <- ifelse(p_lasso > 0.5, 1, 0)
+yhat_ridge <- ifelse(p_ridge > 0.5, 1, 0)
+yhat_elastic <- ifelse(p_elastic > 0.5, 1, 0)
+
+#prediction errors
+err_lasso <- mean(yhat_lasso != y_test)
+err_ridge <- mean(yhat_ridge != y_test)
+err_elastic <- mean(yhat_elastic != y_test)
+
+err_lasso
+err_ridge
+err_elastic
+#should all the values be same?
+
+#sanity check
+cor(p_lasso, p_ridge)
+#            lambda.min
+# lambda.min  0.9991049
+
+cor(p_lasso, p_elastic)
+#            lambda.min
+# lambda.min  0.9999987
+
+# (d) Compare the coefficients across the three penalized models and 
+#comment briefly on differences.
+
+#coefficients
+coef_lasso <- coef(cv_lasso, s = "lambda.min")
+coef_ridge <- coef(cv_ridge, s = "lambda.min")
+coef_elastic <- coef(cv_elastic, alpha = 0.5, s = "lambda.min")
+
+
+
+coef_to_df <- function(coef_obj, model_name) {
+  df <- as.data.frame(as.matrix(coef_obj))
+  colnames(df) <- model_name
+  df$term <- rownames(df)
+  rownames(df) <- NULL
+  df
+}
+
+df_lasso <- coef_to_df(coef_lasso, "LASSO")
+df_ridge <- coef_to_df(coef_ridge, "Ridge")
+df_elastic <- coef_to_df(coef_elastic, "ElasticNet")
+
+coef_compare <- Reduce(
+  function(x, y) merge(x, y, by = "term", all = TRUE),
+  list(df_lasso, df_ridge, df_elastic)
+)
+
+coef_compare
+
+#               term       LASSO         Ridge    ElasticNet
+# 1        (Intercept)  0.24415948  0.3210223077  0.28883836
+# 2      academictrack  0.12829207  0.1392702820  0.13249193
+# 3                age -0.05342270 -0.0557911239 -0.05464121
+# 4       born_germany  0.00000000 -0.0001455997  0.00000000
+# 5               bula -0.01579429 -0.0169943396 -0.01634788
+# 6            deutsch -0.57754970 -0.6181642944 -0.60367971
+# 7             female -0.07889158 -0.0929489715 -0.08472655
+# 8          newspaper  0.05954317  0.0725555902  0.06433384
+# 9  parent_nongermany -0.14778459 -0.1765001754 -0.16299617
+# 10          siblings  0.02569990  0.0475912375  0.03436136
+# 11        sportsclub  0.75966122  0.7384201071  0.75949237
+# 12             urban  0.00000000  0.0095297996  0.00000000
+
+#Interpretation
+
+# The coefficients inferred are consistent across L1, L2
+# and elastic net regularization. Lasso and Elastic net shrink
+# "born_germany" and "urban" to 0 while ridge does not. This indicates
+# limited predictive contribution for both of them. The coefficient on 
+# "sportsclub" is large, positive, and stable across all three models, 
+# indicating a strong association with good health. Overall, the 3 models
+# show similar predictive performance.
 
 
 ## Task 3 #####
@@ -397,9 +507,16 @@ range(min_cvm)
 ### (a) #####
 
 data_clean_tree <- data_clean[, c("health1", controls_unproblematic)]
+
+#perhaps we should use:
+#data_clean_tree <- data_clean[, c("health1", "sportsclub", controls_unproblematic)]
+
 data_clean_tree$health1 <- as.factor(data_clean_tree$health1)
 
-classTree <- tree(formula = health1 ~ ., data = data_clean_tree, subset = train_idx, split = "gini")
+classTree <- tree(formula = health1 ~ ., 
+                  data = data_clean_tree, 
+                  subset = train_idx, 
+                  split = "gini")
 
 summary(classTree)
 
@@ -408,19 +525,32 @@ summary(classTree)
 
 ### (b) #####
 
+#so this will change if treatment is included
 p <- ncol(data_clean_tree) - 1
 
 set.seed(123)
-bag <- randomForest(formula = health1 ~ ., ntree = 500, importance = TRUE, mtry = p, data = data_clean_tree, subset = train_idx)
+bag <- randomForest(formula = health1 ~ ., 
+                    ntree = 500, 
+                    importance = TRUE, 
+                    mtry = p, 
+                    data = data_clean_tree, 
+                    subset = train_idx)
 
-bag_correct <- mean(predict(bag, newdata = data_clean_tree[-train_idx,]) == data_clean_tree[-train_idx, "health1"])
+bag_correct <- mean(predict(bag, 
+                            newdata = data_clean_tree[-train_idx,]) == data_clean_tree[-train_idx, "health1"])
 bag_miss <- 1 - bag_correct 
 
 bag_miss # 0.2974623 - ~ 30%
 
 set.seed(123)
-rF <- randomForest(formula = health1 ~ ., ntree = 500, importance = TRUE, data = data_clean_tree, subset = train_idx)
-rF_correct <- mean(predict(rF, newdata = data_clean_tree[-train_idx,]) == data_clean_tree[-train_idx, "health1"])
+rF <- randomForest(formula = health1 ~ ., 
+                   ntree = 500, 
+                   importance = TRUE, 
+                   data = data_clean_tree, 
+                   subset = train_idx)
+
+rF_correct <- mean(predict(rF, 
+                           newdata = data_clean_tree[-train_idx,]) == data_clean_tree[-train_idx, "health1"])
 rF_miss <- 1 - rF_correct
 
 rF_miss # 0.2738027 - ~ 27%
@@ -428,13 +558,20 @@ rF_miss # 0.2738027 - ~ 27%
 # The random forest has a lower misclassification rate than bagging (by about 2.5 pp)
 
 ### (c) #####
+
+#do not overwrite health1, maybe use a copy?
 data_clean_tree$health1 <- as.numeric(as.character(data_clean_tree$health1))
 
 set.seed(123)
-boost <- gbm(formula = health1 ~ ., distribution = "bernoulli", 
+boost <- gbm(formula = health1 ~ ., 
+             distribution = "bernoulli", 
              data = data_clean_tree[train_idx,], n.trees = 1000)
 
-boost_pred_test <- ifelse(predict(boost, data_clean_tree[-train_idx,], n.trees = 1000, type = "response") > 0.5, 1, 0)
+boost_pred_test <- ifelse(predict(boost, 
+                                  data_clean_tree[-train_idx,], 
+                                  n.trees = 1000, 
+                                  type = "response") > 0.5, 1, 0)
+
 boost_correct <- mean(boost_pred_test == data_clean_tree[-train_idx, "health1"])
 boost_miss <- 1 - boost_correct
 boost_miss #0.2699866
